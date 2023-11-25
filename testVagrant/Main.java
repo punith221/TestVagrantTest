de321b62323422772bb01c09e610ec74c14bc19c
punith221 <punithkumar.221b@gmail.com>
2023-11-25 12:34:44 +0530

    eg

--- testVagrant/Main.java
+++ testVagrant/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HEllo world");
-        for (int i = 0; i < 2; i++) {
-            System.out.println("Hello world");
-            System.out.println("...");
+        
         }
     }
 }
